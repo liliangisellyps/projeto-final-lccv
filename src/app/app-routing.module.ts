f7b9cfe33ae9050b476f25d453f587e75eb4fd5c
Lilian Giselly <lgps@ic.ufal.br>
2021-12-10 20:16:53 -0300

    fix: menu bar working, feat: new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/belogings/components/home/home.component';
+import { RegisterComponent } from './modules/belogings/components/register/register.component';
+import { ListComponent } from './modules/belogings/components/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'bens', component: HomeComponent },
+  { path: 'cadastrar', component: RegisterComponent },
+  { path: 'listar', component: ListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterComponent } from './modules/belogings/components/register/register.component';
+import { HomeComponent } from './modules/belogings/components/home/home.component';
+import { ListComponent } from './modules/belogings/components/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MenuComponent
+    MenuComponent,
+    RegisterComponent,
+    HomeComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule.forRoot(),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
